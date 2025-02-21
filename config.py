ca55c848769c6260f05705eed7387acdc7127dba
NKMODNK1 <160508195+NKMODNK1@users.noreply.github.com>
2025-02-21 21:57:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NKMODNK6/RessoMusic",
+    "https://github.com/NKMODNK1/heruko-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
